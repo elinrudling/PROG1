a8850c4584cb2ab3659701edf22c9918db3b6521
elinrudling <118691648+elinrudling@users.noreply.github.com>
2022-11-28 19:40:50 +0100

    1

--- AssignmentSevenPointOne.java
+++ AssignmentSevenPointOne.java
@@ -3,33 +3,30 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class AssignmentSevenPointOne{
+public class AssignmentSevenPointOne {
     private InputScanner scanner = new InputScanner(System.in);
     private List<Dog> dogList = new ArrayList<>();
 
-    public void registerNewDog(){
-        String dogName = scanner.inputString("Name");
-        dogName = dogName.trim();
-        if (dogName.length() > 0){
-            dogName = dogName.substring(0,1).toUpperCase() + dogName.substring(1).toLowerCase();
-        }
-        else{
-            System.out.println("Error: the name can’t be empty");
-            dogName = scanner.inputString("Name");
-        }
-        String dogBreed = scanner.inputString("Breed");
-        dogBreed = dogBreed.trim();
-        if (dogBreed.length() > 0){
-            dogBreed = dogBreed.substring(0,1).toUpperCase() + dogBreed.substring(1).toLowerCase();
-        }
-        else{
-            System.out.println("Error: the breed can’t be empty");
-            dogBreed = scanner.inputString("Breed");
-        }
+    private String fetchFormattedString(String inputCommand, String errorMessage) {
+        String formattedString;
+        do {
+            formattedString = scanner.inputString(inputCommand).trim();
+            if (formattedString.length() < 1) {
+                System.out.println(errorMessage);
+            }
+        } while (formattedString.length() < 1);
+
+        return formattedString.substring(0, 1).toUpperCase() + formattedString.substring(1).toLowerCase();
+    }
+
+    public void registerNewDog() {
+        String dogName = fetchFormattedString("Name", "Error: the name can’t be empty");
+        String dogBreed = fetchFormattedString("Breed", "Error: the breed can’t be empty");
         int dogAge = scanner.inputInt("Age");
         int dogWeight = scanner.inputInt("Weight");
-
-        Dog dog = new Dog (dogName, dogBreed, dogAge, dogWeight);
+        Dog dog = new Dog(dogName, dogBreed, dogAge, dogWeight);
         dogList.add(dog);
     }
+
+
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 // Elin Rudling elru4802
 
 public class Main {
-    public void main(String[] args){
+    public static void main(String[] args){
         AssignmentSevenPointOne hej = new AssignmentSevenPointOne();
         hej.registerNewDog();
 
