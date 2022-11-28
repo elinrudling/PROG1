b50d015a020129f6ca74dfc1ba061bf074821776
elinrudling <118691648+elinrudling@users.noreply.github.com>
2022-11-28 19:59:21 +0100

    finished 7.1

--- AssignmentSevenPointOne.java
+++ AssignmentSevenPointOne.java
@@ -1,20 +1,17 @@
 // Elin Rudling elru4802
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class AssignmentSevenPointOne {
     private InputScanner scanner = new InputScanner(System.in);
-    private List<Dog> dogList = new ArrayList<>();
+    private ArrayList<Dog> dogList = new ArrayList<>();
 
     private String fetchFormattedString(String inputCommand, String errorMessage) {
-        String formattedString;
-        do {
+        String formattedString = scanner.inputString(inputCommand).trim();
+        while (formattedString.length() < 1) {
+            System.out.println(errorMessage);
             formattedString = scanner.inputString(inputCommand).trim();
-            if (formattedString.length() < 1) {
-                System.out.println(errorMessage);
-            }
-        } while (formattedString.length() < 1);
+        }
 
         return formattedString.substring(0, 1).toUpperCase() + formattedString.substring(1).toLowerCase();
     }
