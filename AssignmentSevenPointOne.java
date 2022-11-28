0187ea602d5272f59a04d7c8809867c8f551896d
elinrudling <118691648+elinrudling@users.noreply.github.com>
2022-11-28 16:03:30 +0100

    ä

--- AssignmentSevenPointOne.java
+++ AssignmentSevenPointOne.java
@@ -13,6 +13,22 @@ public class AssignmentSevenPointOne{
         int dogAge = scanner.inputInt("Age");
         int dogWeight = scanner.inputInt("Weight");
 
+        dogName = dogName.trim();
+        dogName = dogName.substring(0,1).toUpperCase() + dogName.substring(1).toLowerCase();
+
+        dogBreed = dogBreed.trim();
+        dogBreed = dogBreed.substring(0,1).toUpperCase() + dogBreed.substring(1).toLowerCase();
+
+        if (dogName.length() == 0){
+            System.out.println("Error: the name can’t be empty");
+            dogName = scanner.inputString("Name");
+        }
+
+        if (dogBreed.length() == 0){
+            System.out.println("Error: the breed can’t be empty");
+            dogBreed = scanner.inputString("Breed");
+        }
+
         Dog dog = new Dog (dogName, dogBreed, dogAge, dogWeight);
         dogList.add(dog);
     }
