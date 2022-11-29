7daadaaf97185135062d1876cf2e391a4201979c
elinrudling <118691648+elinrudling@users.noreply.github.com>
2022-11-29 16:58:45 +0100

    Update TestData.java

--- TestData.java
+++ TestData.java
@@ -22,6 +22,9 @@ public final class TestData {
 	public static final String READ_DECIMAL_METHOD_NAME = "inputDouble";
 	public static final String MAIN_DOG_LIST_NAME = "dogList";
 	public static final String REGISTER_NEW_DOG_METHOD_NAME = "registerNewDog";
+	public static final String LIST_DOGS_METHOD_NAME = "listDogs";
+	public static final String FIND_DOG_METHOD_NAME = "findDog";
+	public static final String INCREASE_AGE_METHOD_NAME = "increaseAge";
 
 
 	// Här lägger du in ytterligare konstanter när uppgiftsinstruktionerna ber om
