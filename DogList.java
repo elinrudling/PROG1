cb55e573c4158593f6eea3bb602f78d8f27610d6
elinrudling <118691648+elinrudling@users.noreply.github.com>
2022-11-30 15:39:41 +0100

    ö

--- DogList.java
+++ DogList.java
@@ -7,14 +7,25 @@ public class DogList {
     public void addDog(Dog dogToAdd) {
         Dog[] newDogList = new Dog[dogList.length + 1];
         newDogList[dogList.length] = dogToAdd;
-        System.arraycopy(dogList, 0, newDogList, 0, dogList.length);
+        //System.arraycopy(dogList, 0, newDogList, 0, dogList.length);
         dogList = newDogList;
     }
 
-    public void dogToRemove(Dog dogToRemove) {
-        int index = ;
-        dogList = ArrayUtils.remove(dogList, index);
-
+    public void removeDog(Dog dogToRemove) {
+        Dog[] newDogList = new Dog[dogList.length - 1];
+        boolean found = false;
+        for (int i = 0; i < dogList.length; i++){
+            if (!found) {
+                if (dogList[i].getName().equalsIgnoreCase(dogToRemove.getName())){
+                    found = true;
+                    continue;
+                }
+                newDogList[i] = dogList[i];
+            } else {
+                newDogList[i-1] = dogList[i];
+            }
+        }
+        dogList = newDogList;
     }
 
     public Dog dogExists(String nameOfDog) {
--- TestData.java
+++ TestData.java
@@ -24,8 +24,8 @@ public final class TestData {
 	public static final String REGISTER_NEW_DOG_METHOD_NAME = "registerNewDog";
 
 	public static final String DOG_LIST_CLASS_NAME = "DogList";
-	public static final String DOG_LIST_CLASS_ADD_DOG_METHOD_NAME = "dogToAdd";
-	public static final String DOG_LIST_CLASS_REMOVE_DOG_METHOD_NAME = "dogToRemove";
+	public static final String DOG_LIST_CLASS_ADD_DOG_METHOD_NAME = "addDog";
+	public static final String DOG_LIST_CLASS_REMOVE_DOG_METHOD_NAME = "removeDog";
 	public static final String DOG_LIST_CLASS_DOG_EXISTS_METHOD_NAME = "dogExists";
 
 	// Här lägger du in ytterligare konstanter när uppgiftsinstruktionerna ber om
