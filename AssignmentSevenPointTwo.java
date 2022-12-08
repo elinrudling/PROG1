c43044486769a2aa8f86a24ca74747d4d23af31c
elinrudling <118691648+elinrudling@users.noreply.github.com>
2022-12-08 17:25:52 +0100

    finito

--- AssignmentSevenPointTwo.java
+++ AssignmentSevenPointTwo.java
@@ -6,24 +6,6 @@ public class AssignmentSevenPointTwo {
     private InputScanner scanner = new InputScanner(System.in);
     private ArrayList<Dog> dogList = new ArrayList<>();
 
-    private String fetchFormattedString(String inputCommand, String errorMessage) {
-        String formattedString = scanner.inputString(inputCommand).trim();
-        while (formattedString.length() < 1) {
-            System.out.println(errorMessage);
-            formattedString = scanner.inputString(inputCommand).trim();
-        }
-
-        return formattedString.substring(0, 1).toUpperCase() + formattedString.substring(1).toLowerCase();
-    }
-
-    public void registerNewDog() {
-        String dogName = fetchFormattedString("Name", "Error: the name can’t be empty");
-        String dogBreed = fetchFormattedString("Breed", "Error: the breed can’t be empty");
-        int dogAge = scanner.inputInt("Age");
-        int dogWeight = scanner.inputInt("Weight");
-        Dog dog = new Dog(dogName, dogBreed, dogAge, dogWeight);
-        dogList.add(dog);
-    }
     public void listDogs() {
 
         if (dogList.size() == 0) {
--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 // Elin Rudling elru4802
 
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args){
 
