f06f68c62192a7c587a4f211582317657f6860fa
elinrudling <118691648+elinrudling@users.noreply.github.com>
2022-12-09 13:14:02 +0100

    test main

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@
 
 public class Main {
     public static void main(String[] args){
-
+        AssignmentEightPointOne hej = new AssignmentEightPointOne();
+        hej.registerNewOwner();
     }
 
 }
