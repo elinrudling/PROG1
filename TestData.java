11f7828eab72678a20dc38ddd47e91ddbfa8d354
munchosaurus <70525907+munchosaurus@users.noreply.github.com>
2023-01-05 16:47:19 +0100

    ö

--- TestData.java
+++ TestData.java
@@ -24,5 +24,9 @@ public final class TestData {
 
 	public static final String ADD_DOG_TO_OWNER_METHOD_NAME = "addDogToOwner";
 	public static final String OWNS_DOG_METHOD_NAME = "checkIfOwnerOwnsDog";
+	
+	public static final String REMOVE_OWNED_DOG_METHOD_NAME = "removeDog";
+	public static final String REMOVE_DOG_FROM_OWNER_METHOD_NAME = "removeDogFromOwner";
+	public static final String REMOVE_OWNER_OF_DOG_METHOD_NAME = "removeOwnerFromDog";
 
 }
